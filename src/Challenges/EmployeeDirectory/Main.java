93efd447cae4d314d92e4532b8979cf7d19d4c3e
Adam Philip <adamphilip9@gmail.com>
2026-02-28 03:39:04 +0530

    Challenges

---
+++ src/Challenges/EmployeeDirectory/Main.java
@@ -0,0 +1,18 @@
+package Challenges.EmployeeDirectory;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args){
+
+        HashMap<Integer, String> employeeDirectory = new HashMap<>();
+
+        employeeDirectory.put(101, "Adam");
+        employeeDirectory.put(102, "Eve");
+        employeeDirectory.put(103, "Kevin");
+
+        System.out.println(employeeDirectory.get(101));
+
+        System.out.println(employeeDirectory.containsKey(104));
+    }
+}
